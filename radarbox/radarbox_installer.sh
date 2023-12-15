4447c05bfa23973e5029c4fa666489bea090591e
Hugo Romano <hugoromano@gmail.com>
2023-12-15 04:49:28 +0000

    Update Dockerfile and installer script for RadarBox

--- radarbox/radarbox_installer.sh
+++ radarbox/radarbox_installer.sh
@@ -2,24 +2,29 @@
 set -e
 
 # Import our key to apt-key
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 1D043681
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 1D043681
 
 # Move old source
 /bin/rm -f /etc/apt/sources.list.d/rb24.list
 
 # Create a new debian repository source file
-echo 'deb https://apt.rb24.com/ buster main' > /etc/apt/sources.list.d/rb24.list
+echo 'deb https://apt.rb24.com/ bullseye main' > /etc/apt/sources.list.d/rb24.list
 
 arch="$(dpkg --print-architecture)"
 echo System Architecture: $arch
 
-# If host architecture is i386 or amd64, install armhf-version of RadarBox and run it throug software emulation.
+# If host architecture is i386, amd64, or arm6 install armhf-version of RadarBox and run it through software emulation.
 if [ "$arch" = "i386" ] || [ "$arch" = "amd64" ]; then 
 	dpkg --add-architecture armhf
-		apt update && apt install -y --no-install-recommends \
+		apt update  && apt install -y --no-install-recommends \
 	    rbfeeder:armhf qemu-user qemu-user-static binfmt-support libc6-armhf-cross
+# This elif is hopefully a temporary workaround, as the newest RadarBox library is not currently available for arm64.	    
+elif [ "$arch" = "arm64" ]; then 
+	dpkg --add-architecture armhf
+		apt update  && apt install -y --no-install-recommends \
+	    rbfeeder:armhf
 else 
-	apt update && apt install -y --no-install-recommends \
+    apt update && apt install -y --no-install-recommends \
 	    rbfeeder
 fi
 
